21613f12762ca6ccac1f45a7b657e2feebe6cf5a
kimseyoung <tpdud66770@naver.com>
2025-12-05 14:46:14 +0900

    세영 : 전체 통합본

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
-import Detail from "./pages/detail";
 import Login from "./pages/login";
 import Join from "./pages/join";
-import Register from "./pages/register";
-import Update from "./pages/update";
-import MyPage from "./pages/mypage";
-import Aimg from "./pages/ailmg";
+
 
 function App() {
   return (
